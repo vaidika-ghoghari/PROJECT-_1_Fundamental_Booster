d467dbae04939ef9efce65050659ad182900ea55
Vaidika_ghoghari <ghogharivaidika@gmail.com>
2026-09-09 13:22:48 +0530

    Refactor personal data collector for clarity and structure

--- project1.py
+++ project1.py
@@ -1,19 +1,53 @@
-print("Welcome to the Interactive Personal Data Collecter ! ")
+print("\nWelcome to the Interactive Personal Data Collecter ! ")
 
-name=input("Please enter your name : ")
+#take a name from user
+name=input("\nPlease enter your name : ")
+
+#take a age from user
 age =int(input("Please enter your age : "))
+
+#take a height from user
 height=float(input("Please enter Your height in meters : "))
+
+#take a favourite number from user
 fav_num=int(input("Please enter your favourite number : "))
 
-print("\n Thank you! Here is the information we collected :")
+print("--"*50)
+#print a thank you message for inputing information
+print("Thank you! Here is the information we collected :")
 
-print("\Name : ",name, "  (type : ",type(name),", Memory Address : ",id(name),")")
-print("\n Age : ",age, "  (type : ",type(age),", Memory Address : ",id(age),")")
-print("\n Height : ",height, "  (type : ",type(height),", Memory Address : ",id(height),")")
-print("\n Favourite Number : ",fav_num, "  (type : ",type(fav_num),", Memory Address : ",id(fav_num),")")
 
-present_year=2026
-year_of_birth=present_year-age
-print(f"\nYour birth year is approximately :{year_of_birth} (based on your age of {age} ) ")
+#print a user's detail with the data type and memory address
+print("\nName : ",name)
+print("Data Type : ",type(name))
+print("Memory Address : ",id(name))
 
-print("\n Thank you for using the Personal Data Collecter. Goodbye !")
+print(" \nAge : ",age)
+print("Data Type : ",type(age))
+print("Memory Address : ",id(age))
+
+print("\nHeight : ",height)
+print("Data Type : ",type(height))
+print("Memory Address : ",id(height))
+
+print("\nFavourite Number : ",fav_num)
+print("Data Type : ",type(fav_num))
+print("Memory Address : ",id(fav_num))
+
+'''
+#print user details  with f-string
+print(f"\nName : {name}  ,  (Data Type  :  {type(name) } )  ,  (Memory Address  :  {id(name)})")
+print(f"Age : {age}  ,  (Data Type  :  {type(age) } )  ,  (Memory Address  :  {id(age)})")
+print(f"Height : {height}  ,  (Data Type  :  {type(height) } )  ,  (Memory Address  :  {id(height)})")
+print(f"Favourite Number  : {fav_num}  ,  (Data Type  :  {type(fav_num) } )  ,  (Memory Address  :  {id(fav_num)})")
+'''
+
+print("--"*50)
+#calculation of birth year
+current_year=2026
+year_of_birth=current_year-age
+#print a birth year
+print(f"Your birth year is approximately :{year_of_birth} (based on your age of {age} ) ")
+
+#print a final thank you message 
+print("\nThank you for using the Personal Data Collecter. Goodbye !")
